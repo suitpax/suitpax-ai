ad9e936805389e0f48a99c93f37b8e570056f01b
Alberto Zurano <alberto.zurano@suitpax.com>
2025-08-04 09:10:56 +0200

    Create generate-pdf/route.ts

---
+++ app/api/generate-pdf/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server"
+import { generatePDF } from "@/lib/generatePDF" // Ajusta la ruta
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json()
+    const buffer = await generatePDF(body)
+
+    return new Response(buffer, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": 'attachment; filename="documento.pdf"',
+      },
+    })
+  } catch (error) {
+    return new Response("Error generando PDF", { status: 500 })
+  }
+}
